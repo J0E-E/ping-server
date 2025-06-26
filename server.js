5b75f525b9f07d54acab81891e8a7fc94fa58198
Joey Iglesias <jiglesias@hbham.com>
2025-06-26 10:43:54 -0400

    Establishes basic WebSocket server
    Sets up a basic WebSocket server using the 'ws' library.
    Implements connection handling with unique identifiers.
    Adds message handling and player lobby management.

--- server.js
+++ server.js
@@ -4,25 +4,77 @@ import { parse } from 'url';
 
 const wss = new WebSocketServer({port: 3000})
 
-const playerLobby = new Map()
+const playerSet = new Map()
+
+function broadcastLobbyUpdate() {
+	let players = Array.from(playerSet.values())
+	const lobbyUpdateMessage = JSON.stringify({type: 'lobby', action: 'update-players', players})
+
+	wss.clients.forEach((client) => {
+		if (client.readyState === client.OPEN) {
+			client.send(lobbyUpdateMessage)
+		}
+	})
+}
 
 wss.on('connection', (ws, req) => {
 	const params = new URLSearchParams(parse(req.url, true).query)
+
 	let connectionId = params.get('connectionId')
 	if (!connectionId) {
 		connectionId = uuidv4();
 	}
-	const joinMessage = JSON.stringify({type: "connect", connectionId})
-	ws.send(joinMessage)
+	ws.id = connectionId;
 
+	const joinMessage = JSON.stringify({type: "connection", connectionId})
+
+	ws.send(joinMessage)
 	console.log('Client connected.')
 
 	ws.on('message', (data) => {
-		const message = data.toString()
-		console.log('Received message: ', message)
-
-		ws.send(`Echo: ${message}`)
+		let message = JSON.parse(data.toString())
+		messageHandler(message, ws)
 	})
 
+	ws.on('close', (code, reason) => {
+		console.log(`Client disconnected. Code: ${code}, Reason: ${reason.toString()}`);
+		const player = playerSet.get(ws.id)
+		if (player) {
+			// TODO: handle with a timeout. Allow for reconnect in x amount of time.
+			console.log(`Player Removed: ${player.userName}-${player.playerId}`)
+			playerSet.delete(ws.id)
+			broadcastLobbyUpdate()
+		}
+	});
 
+	ws.on('error', (error) => {
+		console.error('WebSocket error:', error);
+	});
 })
+
+
+const messageHandler = (message, ws) => {
+	switch (message.type){
+		case 'lobby':
+			lobbyMessageHandler(message, ws)
+			break;
+		default:
+			console.log("NOT SUPPORTED, BITCH!")
+	}
+}
+
+const lobbyMessageHandler = (message, ws) => {
+	switch (message.action) {
+		case 'joining':
+			// TODO: have legit login/userId/Auth system.
+			let playerId = uuidv4()
+			let playerData = {playerId, userName: message.userName, playerLocation: 'lobby', rating: '12/100'}
+			playerSet.set(message.connectionId, playerData)
+			const loggedInMessage = JSON.stringify({type: 'lobby', action: 'logged-in', playerData})
+			ws.send(loggedInMessage)
+			broadcastLobbyUpdate()
+			break;
+		default:
+			console.error(`Unsupported Lobby Action: ${message.action}`)
+	}
+}
